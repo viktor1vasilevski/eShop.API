cd19e0ab90bc1eb4aaf684ffd6a6121955989c94
Viktor <vasilevskiviktor@yahoo.com>
2025-05-14 00:44:52 +0200

    minor changes

--- Admin.eShop/Controllers/AdminAuthController.cs
+++ Admin.eShop/Controllers/AdminAuthController.cs
@@ -3,20 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 using Main.Requests.Auth;
 using Microsoft.AspNetCore.Authorization;
 
-namespace Admin.eShop.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    [AllowAnonymous]
-    public class AdminAuthController(IAdminAuthService authService) : BaseController
-    {
-        private readonly IAdminAuthService _adminAuthService = authService;
+namespace Admin.eShop.Controllers;
 
-        [HttpPost("login")]
-        public async Task<IActionResult> LoginAsync([FromBody] UserLoginRequest request)
-        {
-            var response = await _adminAuthService.AdminLoginAsync(request);
-            return HandleResponse(response);
-        }
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class AdminAuthController(IAdminAuthService authService) : BaseController
+{
+    private readonly IAdminAuthService _adminAuthService = authService;
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] UserLoginRequest request)
+    {
+        var response = await _adminAuthService.AdminLoginAsync(request);
+        return HandleResponse(response);
     }
 }
